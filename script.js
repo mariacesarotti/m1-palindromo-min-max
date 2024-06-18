98eddfc2e610b3fb7e4338915fb54242117f1f33
Maria Cesarotti <mariacesarotti@gmail.com>
2024-06-18 15:42:58 -0300

    first try

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase){
+  let fraseRecebida = frase;
+  let tamanhoFrase = fraseRecebida.length;
+  let indiceInversao = tamanhoFrase;
+  let consoleImpressao = fraseRecebida.charAt(tamanhoFrase-1);
+
+
+
+
+
+  
+  for(let i = 1; i < tamanhoFrase; i++){
+    let letraAoContrario = fraseRecebida.charAt(indiceInversao - 2);
+    consoleImpressao += letraAoContrario;
+    indiceInversao = tamanhoFrase - 1;
+  }
+  console.log(consoleImpressao);
+  if(consoleImpressao == fraseRecebida){
+    return true;
+  }else{
+    return false;
+  }
 }
+
+
+console.log(isPalindrome("ana"));
+
+
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
