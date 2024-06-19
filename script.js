408fd00b463b941be41ce23d3589b9557e2838ac
Maria Cesarotti <mariacesarotti@gmail.com>
2024-06-19 17:47:06 -0300

    fix5

--- script.js
+++ script.js
@@ -18,18 +18,41 @@ function isPalindrome(frase){
 
 
 
-
 function arrayMaxMin(arr){
-    let arrayRecebida = arr;
-    let arrayTamanho = arrayRecebida.length;
-    let numeroMaior = arrayRecebida[0];
-    let numeroMenor = arrayRecebida[1];
-    let arrayResultado = numeroMenor + "" + numeroMaior;
+  const arrayRecebida = arr;
+  let tamanhoArray = arrayRecebida.length;
+  let arrayMin = arrayRecebida[0];
+  let arrayMax = arrayRecebida[1];
 
-    for(let i = 0; i > arrayTamanho - 1; i++){
-        if(numeroMaior > numeroMenor){
-          numeroMenor = arrayRecebida[arrayTamanho - i]; 
-        }
+  for(let i = tamanhoArray - 1; i < 0; i--){
+    if(arrayMin > arrayMax){
+      arrayMax = arr[i];
+    }else{
+      arrayMin[i];
     }
-    return arrayResultado;
+  }
+  let consoleLogImpressao = arrayMax + "," + arrayMin;
+  return consoleLogImpressao;
 }
+
+
+
+
+// function arrayMaxMin(arr){
+//     let arrayRecebida = arr;
+//     let arrayTamanho = arrayRecebida.length;
+//     let numeroMaior = arrayRecebida[0];
+//     let numeroMenor = arrayRecebida[1];
+//     let arrayResultado = numeroMaior + "," + numeroMenor;
+
+//     for(let i = 0; i > arrayTamanho - 1; i++){
+//         while(numeroMaior > numeroMenor){
+//         numeroMenor = arrayRecebida[arrayTamanho - i];   
+//         if(numeroMenor > numeroMaior){
+//           numeroMaior = arrayRecebida[arrayTamanho - i];  
+//         }  
+//       }
+//     }
+//     return arrayResultado;
+//   }
+  
